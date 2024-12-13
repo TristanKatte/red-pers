7ecf0d27fac01ef84a8fcf90529c470e0434ba53
TristanKatte <tristankatte@hotmail.com>
2024-12-13 04:29:06 +0100

    added transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,19 @@
-<link rel="stylesheet" href="/global.css">
-
-<slot></slot>
 <script>
-      import { onNavigate } from '$app/navigation'
+    import { onNavigate } from '$app/navigation'
 
 onNavigate ((navigation) => {
 if (!document.startViewTransition) return
 
 return new Promise(resolve => {
-    document.startViewTransition(async () => {
-        
-        resolve()
-        await navigation.complete()
-    });
+  document.startViewTransition(async () => {
+      
+      resolve()
+      await navigation.complete()
+  });
 });
 });
 </script>
+<link rel="stylesheet" href="/global.css">
 
-<style>
-    @keyframes fade-in {
-    from { opacity: 0; }
-  }
-  
-  @keyframes fade-out {
-    to { opacity: 0; }
-  }
-  
-  @keyframes slide-from-right {
-    from { transform: translateX(100%); }
-  }
-  
-  @keyframes slide-to-left {
-    to { transform: translateX(-100%); }
-  }
-  
-  ::view-transition-old(root) {
-    animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
-      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
-  }
-  
-  ::view-transition-new(root) {
-    animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
-      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
-  }
-</style>
+<slot></slot>
+
